2444dd9056043efcda0475a00affd838279fc20d
Harsh Makani <harshvardhanmakani@gmail.com>
2019-02-24 17:42:41 -0600

    Updates

---
+++ precache-manifest.1168bf4b9468a3e35c0340444e12ca37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6615e44620fa0b084e5",
+    "url": "/audio-player-app/static/js/runtime~main.f6615e44.js"
+  },
+  {
+    "revision": "4156401c888a09b78e37",
+    "url": "/audio-player-app/static/js/main.4156401c.chunk.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/audio-player-app/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "4156401c888a09b78e37",
+    "url": "/audio-player-app/static/css/main.439e8c0b.chunk.css"
+  },
+  {
+    "revision": "d54812806b68ee3309406ec8b601c46d",
+    "url": "/audio-player-app/index.html"
+  }
+];
